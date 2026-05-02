dc59b58c38b43414b315b3df7d5b19ed9894980f
Carlos Lott <celotts@gmail.com>
2026-05-02 15:25:26 -0600

    Initial commit: Spring Boot POS project with Gradle

---
+++ src/main/java/com/celotts/pos/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.celotts.pos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "API is running";
+    }
+}
